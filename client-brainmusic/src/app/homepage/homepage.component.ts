0b2a270461a36bf358bcde2f7bbf0baf9ba9a333
thefourhorsemen-4 <fourhorsemen987@gmail.com>
2019-03-15 09:53:13 +0700

    change background player

--- client-brainmusic/src/app/homepage/homepage.component.ts
+++ client-brainmusic/src/app/homepage/homepage.component.ts
@@ -1,19 +1,42 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
-export class HomepageComponent implements OnInit {
+export class HomepageComponent implements OnInit, OnDestroy {
 
   constructor() {
   }
 
+  audio: any;
+
   ngOnInit() {
+    this.audio = new Audio();
+    this.audio.src = 'https://stream.brain.fm/?tkn=1e70ce40-3fc4-11e9-a257-77080ead160f-u999628';
   }
 
+  setVolume(vol) {
+    this.audio.volume = vol;
+  }
 
+  playTrack() {
+    this.audio.play();
+    console.log('play');
+  }
+
+  pauseTrack() {
+    this.audio.pause();
+    console.log('pause');
+  }
+
+  ngOnDestroy() {
+    if (this.audio) {
+      this.audio.pause();
+      this.audio = null;
+    }
+  }
 }
 
 
